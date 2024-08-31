a7a26632e82497746181ae8ef8336c331c6a8303
suhaneeAhirwar <suhaniahirwar930@gmail.com>
2024-08-31 21:18:15 +0530

    first commit

---
+++ Qtwo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Qtwo {
+    int num;
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+         int num=scanner.nextInt();
+         //this  is condition 
+         if(num>0){
+            System.out.println("number is positive");
+         }
+         else{
+            System.out.println("number is negative");
+         }
+    }
+    
+}
