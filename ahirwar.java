2e58b091fd8211d979f852c6a2caf700a6e17da4
suhaneeAhirwar <suhaniahirwar930@gmail.com>
2024-09-02 15:24:38 +0530

    first commit

---
+++ ahirwar.java
@@ -0,0 +1,17 @@
+public class  ahirwar {
+    public static void main(String[] args) {
+        int n = 5; // number of rows
+        for (int i = 1; i <= n; i++) {
+            // Print leading spaces
+            for (int j = i; j < n; j++) {
+                System.out.print("  ");
+            }
+            // Print stars
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+ 
+    }
