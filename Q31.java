331a9d2b7aabf7b0981c9dcfe5700d6e2a6ba267
suhaneeAhirwar <suhaniahirwar930@gmail.com>
2024-09-03 15:32:12 +0530

    first commit

---
+++ Q31.java
@@ -0,0 +1,15 @@
+class  print {
+        
+        int  n=10;
+         print (){
+            System.out.println(" number " +n);
+         }
+
+
+    
+}
+public class Q31{
+    public static void main(String[] args) {
+         print obj=new  print ();
+    }
+}
