4abf61c3d6690c81f0543b6161204d25b3316dd3
suhaneeAhirwar <suhaniahirwar930@gmail.com>
2024-09-03 14:50:22 +0530

    first commit

---
+++ Q14.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Q14 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int N = sc.nextInt();
+        
+        for(n=1; n<=N; n++){
+            if(n%10==0){
+                 
+                System.out.println(n+" multiple of 10");
+              //  System.out.println("multiple of 10");
+            }
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+}
