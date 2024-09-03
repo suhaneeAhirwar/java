359cdb3980bc243b19f1e1fef2cb8a6bbd4adbf6
suhaneeAhirwar <suhaniahirwar930@gmail.com>
2024-09-03 15:35:01 +0530

    first commit

---
+++ Q32.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class v {
+  static   String a; 
+  static  String b;
+    //v(){
+       // public static void main(String args[]){
+
+      //  }
+       // a=" shivani";
+       // b="suhanee";
+        //System.out.println( a+" "+b);
+        // this is parametrized constuctor 
+   v(String  d , String c){
+    a=d;
+    b=c;
+    System.out.println(  d +" " +c);  
+   }
+    
+}     
+public class Q32 {
+    public static void main(String args[]){
+
+    v r=new v( " shivani", "Karan ahirwar");
+
+    }
+
+}
