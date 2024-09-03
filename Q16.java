1aab27074d565eafeddc78a23bfe875f8872a02c
suhaneeAhirwar <suhaniahirwar930@gmail.com>
2024-09-03 15:20:46 +0530

    first commit

---
+++ Q16.java
@@ -0,0 +1,28 @@
+
+    import java.util.Scanner;
+
+public class Q16 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        // Calculate the HCF using the Euclidean algorithm
+        int hcf = findHCF(num1, num2);
+
+        System.out.println("The HCF of " + num1 + " and " + num2 + " is: " + hcf);
+    }
+
+    // Function to find HCF using Euclidean algorithm
+    public static int findHCF(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
+
