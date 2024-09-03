1aae4ad46075f2953dde74bcdc340b5b1cf19f01
suhaneeAhirwar <suhaniahirwar930@gmail.com>
2024-09-03 15:43:55 +0530

    first commit

---
+++ Qthree.java
@@ -0,0 +1,19 @@
+public class Qthree {
+    public static void main(String [] args){
+        int num1=20;
+        int num2=9;
+        int num3=12;
+        int Maxval=num1;
+if(num2>Maxval){
+    Maxval=num2;
+}
+if(num3>Maxval){
+    Maxval=num3;
+}
+System.out.println("maximum number"+ Maxval);
+
+        }
+    
+    }
+
+    
