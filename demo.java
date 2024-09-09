b67bfc2a6c44ff35db664e5024b0c3d7d2c7094b
suhaneeAhirwar <suhaniahirwar930@gmail.com>
2024-09-09 16:17:31 +0530

    first commit

---
+++ demo.java
@@ -0,0 +1,17 @@
+public class demo {
+    public static void main(String [] args){
+        System.out.println( "size of bits="+  Byte. MIN_VALUE);
+        System.out.println( "size of bytes="+ Integer. MAX_VALUE);
+        System.out.println("size of bytes=" + Byte.BYTES);
+        System.out.println("size of bytes=" + Byte.BYTES);
+        System.out.println( "size of bytes="+ Double.SIZE);
+        System.out.println("size of bytes=" + Double.BYTES);
+        System.out.println( "size of bytes="+  Short.SIZE);
+        System.out.println("size of bytes=" + Short.BYTES);
+        System.out.println( "size of bytes="+ Float.SIZE);
+        System.out.println("size of bytes=" + Float.BYTES);
+        System.out.println( "size of bytes="+ Character.SIZE);
+        System.out.println("size of bytes=" + Character.BYTES);
+    }
+    
+}
